f6d3a8a11b0a11f02ad36d213be7da5ce6c24541
Илья Аленин <alenin-ru@yandex.ru>
2021-01-23 22:18:41 +0300

    Many changes

--- app/src/main/java/com/example/mufia/Game.java
+++ app/src/main/java/com/example/mufia/Game.java
@@ -3,9 +3,11 @@ package com.example.mufia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckedTextView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,7 +18,7 @@ public class Game extends AppCompatActivity {
 
     int nigth = 0;
     ArrayList<Member> members = new ArrayList<>();
-
+    ArrayList<Roles> roles = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,23 +26,81 @@ public class Game extends AppCompatActivity {
 
         readMembers();
 
+        readRoles();
+
+        aliveLayout();
+
+        firstDay();
+
     }
 
-    private void readMembers() {
+    private void readRoles() {
         try {
-            LinearLayout alive_layout = findViewById(R.id.alive_layout);
             BufferedReader br = new BufferedReader(new InputStreamReader(
-                    openFileInput("cur_members.txt")));
+                    openFileInput("cur_roles.txt")));
             String word;
             while ((word = br.readLine()) != null) {
-                CheckedTextView name = new CheckedTextView( this);
-                name.setCheckMarkDrawable(android.R.drawable.checkbox_off_background);
-                name.setText(word);
-                name.setTextSize(24);
-                alive_layout.addView(name);
+                String role = word;
+                int num = Integer.parseInt(br.readLine());
+                roles.add(new Roles(role, num));
             }
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    private void firstDay() {
+
+    }
+
+    private void aliveLayout() {
+
+        LinearLayout alive_layout = findViewById(R.id.alive_layout);
+
+        for(int i = 0; i < members.size(); i++) {
+            CheckedTextView name = new CheckedTextView(this);
+            name.setCheckMarkDrawable(android.R.drawable.checkbox_off_background);
+            name.setChecked(false);
+            name.setText(members.get(i).name);
+            name.setTextSize(24);
+            name.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (((CheckedTextView) view).isChecked()) {
+                        ((CheckedTextView) view).setCheckMarkDrawable(android.R.drawable.checkbox_off_background);
+                        ((CheckedTextView) view).setChecked(false);
+
+                    } else {
+                        uncheckAll();
+                        ((CheckedTextView) view).setCheckMarkDrawable(android.R.drawable.checkbox_on_background);
+                        ((CheckedTextView) view).setChecked(true);
+                    }
+                }
+            });
+
+            alive_layout.addView(name);
+        }
+    }
+
+    private void readMembers() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    openFileInput("cur_members.txt")));
+            String word;
+            while ((word = br.readLine()) != null) {
+                members.add(new Member(word));
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void uncheckAll() {
+        LinearLayout alive_layout = findViewById(R.id.alive_layout);
+        for(int i = 0; i < alive_layout.getChildCount(); i++){
+            ((CheckedTextView) alive_layout.getChildAt(i)).setCheckMarkDrawable(android.R.drawable.checkbox_off_background);
+            ((CheckedTextView) alive_layout.getChildAt(i)).setChecked(false);
+        }
+
     }
 }
--- app/src/main/java/com/example/mufia/RolesList.java
+++ app/src/main/java/com/example/mufia/RolesList.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class RolesList extends AppCompatActivity {
+    int num_members = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +27,19 @@ public class RolesList extends AppCompatActivity {
 
         LinearLayout roles_layout = findViewById(R.id.roles_layout);
 
+        num_members = numRoles();
         Button to_game = findViewById(R.id.to_game);
         to_game.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RolesList.this, Game.class);
-                startActivity(intent);
+
+                if(correctRoles()) {
+                    writeRoles();
+                    Intent intent = new Intent(RolesList.this, Game.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(RolesList.this, "Количество ролей не соответсвует количсеству игроков", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -47,5 +59,49 @@ public class RolesList extends AppCompatActivity {
         }catch (IOException e){
             e.printStackTrace();
         }*/
+    }
+
+    private int numRoles() {
+
+        int num_members_file = 0;
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    openFileInput("cur_members.txt")));
+            String word;
+            while ((word = br.readLine()) != null) {
+                num_members_file++;
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return num_members_file;
+    }
+
+    private boolean correctRoles() {
+        LinearLayout roles_layout = findViewById(R.id.roles_layout);
+
+        int num_roles_seekbar = 0;
+        LinearLayout num_layout = findViewById(R.id.num_layout);
+        for (int i = 0; i < roles_layout.getChildCount(); i++) {
+            num_roles_seekbar -= ((SeekBar) num_layout.getChildAt(i)).getProgress() ;
+        }
+        return num_members == num_roles_seekbar;
+    }
+
+    private void writeRoles() {
+        LinearLayout roles_layout = findViewById(R.id.roles_layout);
+        LinearLayout num_layout = findViewById(R.id.num_layout);
+        try{
+            //FileOutputStream fw = new FileOutputStream(getFilesDir() + "/" +"cur_members.txt", false);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                    openFileOutput("cur_roles.txt", MODE_PRIVATE)));
+            for (int i = 0; i < roles_layout.getChildCount(); i++) {
+                bw.write((((TextView) roles_layout.getChildAt(i)).getText().toString() + '\n'));
+                bw.write(Integer.toString(((SeekBar) num_layout.getChildAt(i)).getProgress()) + '\n');
+            }
+            bw.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
